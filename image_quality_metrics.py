bf464550e168e39f0263ae09a5db8e8f1139304d
Atheesh Krishnan <atheesh.98@gmail.com>
2019-08-01 14:16:33 +0530

    Initial Commit

---
+++ image_quality_metrics.py
@@ -0,0 +1,33 @@
+import math
+import numpy as np
+
+# define a function for peak signal-to-noise ratio (PSNR)
+def psnr(target, ref):
+         
+    # assume RGB image
+    target_data = target.astype(float)
+    ref_data = ref.astype(float)
+
+    diff = ref_data - target_data
+    diff = diff.flatten('C')
+
+    rmse = math.sqrt(np.mean(diff ** 2.))
+
+    return 20 * math.log10(255. / rmse)
+
+# define function for mean squared error (MSE)
+def mse(target, ref):
+    # the MSE between the two images is the sum of the squared difference between the two images
+    err = np.sum((target.astype('float') - ref.astype('float')) ** 2)
+    err /= float(target.shape[0] * target.shape[1])
+    
+    return err
+
+# define function that combines all three image quality metrics
+def compare_images(target, ref):
+    scores = []
+    scores.append(psnr(target, ref))
+    scores.append(mse(target, ref))
+    scores.append(ssim(target, ref, multichannel =True))
+    
+    return scores
---
+++ main.py
@@ -0,0 +1,14 @@
+import sys
+import keras
+import cv2
+import math
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from skimage.measure import compare_ssim as ssim
+# neural network architecture
+from keras.models import Sequential
+from keras.layers import Conv2D
+from keras.optimizers import Adam
+# misc
+from image_quality_metrics import *
